5f39b6596355ce7e033478a58b41d8f18e44e22d
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-27 18:48:35 +0800

    Update bis_get_floor_price.py

--- bis_get_floor_price.py
+++ bis_get_floor_price.py
@@ -7,7 +7,7 @@ import datetime
 url = "https://api.bestinslot.xyz/v3/collection/market_info"
 
 headers = {
-    'x-api-key': os.environ.get('MY_BIS_KEY') # 使用環境變數中的 MY_BIS_KEY
+    'x-api-key': os.environ.get('MY_BIS_KEY') # 使用环境变量中的 MY_BIS_KEY
 }
 
 params = {
@@ -15,11 +15,14 @@ params = {
 }
 
 def get_floor_price():
-    # Set default file_path if it is None
-    default_file_path = "floor_prices.json"
+    # Set file_path to 'floor_prices.json' in the repository's root directory
+    file_path = os.path.join(os.getenv('GITHUB_WORKSPACE'), "floor_prices.json")
+
+    # Set default values if folder_path and file_path are None
+    
+    current_data = []
 
     
-
     response = requests.get(url, headers=headers, params=params)
     json_data = response.json()
 
@@ -27,7 +30,7 @@ def get_floor_price():
         min_price = round(json_data['data']['floor_price']/100000000, 4)
 
     try:
-        with open(default_file_path, "r") as file:
+        with open(file_path, "r") as file:
             current_data = json.load(file)
     except FileNotFoundError:
         current_data = []
@@ -36,12 +39,10 @@ def get_floor_price():
     current_timeformat = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
 
     new_entry = {"time": current_time, "value": min_price}
-
     current_data.insert(0, new_entry)
 
-    with open(default_file_path, "w") as file:
+    with open(file_path, "w") as file:
         json.dump(current_data, file, indent=None)
 
-# Call the function with default file_path if the script is directly executed
 if __name__ == "__main__":
     get_floor_price()
