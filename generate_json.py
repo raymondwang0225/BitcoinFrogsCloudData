aeb91261a65d7da23bb2a198dcb7163537bbaf5c
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-28 12:02:17 +0800

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -11,11 +11,14 @@ def generate_json(filename):
     # Check if the JSON file already exists
     if os.path.exists(filename):
         with open(filename, 'r') as f:
-            existing_data = json.load(f)
-            data['previous_random_number'] = existing_data.get('random_number')
+            try:
+                existing_data = json.load(f)
+                data['previous_random_number'] = existing_data.get('random_number')
+            except json.JSONDecodeError:
+                pass
 
     with open(filename, 'w') as f:
         json.dump(data, f, indent=4)
 
 if __name__ == "__main__":
-    generate_json("data.json")
+    generate_json("BitcoinFrogsCloudData/data.json")
