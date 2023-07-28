1f9cdceb16ed9fd9514dce76358177914d9efb00
raymondwang0225 <100661872+raymondwang0225@users.noreply.github.com>
2023-07-28 11:07:20 +0800

    Create generate_json.py

---
+++ generate_json.py
@@ -0,0 +1,21 @@
+import json
+import random
+import os
+
+def generate_json(filename):
+    data = {
+        "random_number": random.randint(1, 100),
+        "message": "Hello, World!"
+    }
+
+    # Check if the JSON file already exists
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            existing_data = json.load(f)
+            data['previous_random_number'] = existing_data.get('random_number')
+
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+if __name__ == "__main__":
+    generate_json("data.json")
